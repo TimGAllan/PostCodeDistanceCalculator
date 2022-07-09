5d3a8be1eb71127ecd39a50a517ff2fc5f17c3bb
TimGAllan <59513050+TimGAllan@users.noreply.github.com>
2022-07-09 14:32:42 +0100

    Added Distance to all Post Codes Table Function

---
+++ PostCodeDistanceCalculator/Functions/distanceToAllPostCodes.sql
@@ -0,0 +1,20 @@
+﻿CREATE FUNCTION distanceToAllPostCodes
+(
+	@postcode varchar(10)
+)
+RETURNS @t TABLE
+(
+	 postcode varchar(10)
+	,distance float
+)
+AS
+BEGIN
+	INSERT @t
+	SELECT 
+
+	  a.postcode
+	 ,dbo.distanceBetweenPostCodes(@postCode,a.postcode) As Distance
+
+	FROM PostCodes a
+	RETURN
+END
